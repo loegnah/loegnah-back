30f6524db4e899d264cf2dd3745dce79d77cb412
loegnah <loegnah@gmail.com>
2024-01-14 00:54:44 +0900

    chore: Apply onModuleInit

--- src/infra/infra.service.ts
+++ src/infra/infra.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import TelegramBot from 'node-telegram-bot-api';
 
 import { Env } from '#/config/configuration';
 import { TelegramService } from '#/telegram/telegram.service';
@@ -8,21 +9,31 @@ import { TelegramService } from '#/telegram/telegram.service';
 const logger = new Logger('Infra');
 
 @Injectable()
-export class InfraService {
-  private publicIpApiUrl = this.config.get<string>('publicIpApiUrl');
+export class InfraService implements OnModuleInit {
+  private readonly PUBLIC_IP_API_URL =
+    this.config.get<string>('publicIpApiUrl');
+  private publicIp: string;
+  private bot: TelegramBot;
 
   constructor(
     private http: HttpService,
     private config: ConfigService<Env, true>,
     private telegramService: TelegramService,
-  ) {
-    this.initTelegramBot();
+  ) {}
+
+  async onModuleInit() {
+    await this.initTelegramBot();
+    this.publicIp = await this.getPublicIp();
   }
 
   private async initTelegramBot() {
-    const bot = this.telegramService.makeBot({
+    this.bot = this.telegramService.makeBot({
       token: this.config.get('telegramBotToken'),
     });
+    await this.setTelegramBotListener(this.bot);
+  }
+
+  private async setTelegramBotListener(bot: TelegramBot) {
     bot.onText(/\/show (.+)/, async (msg, match) => {
       const chatId = msg.chat.id;
       if (!match) return;
@@ -38,15 +49,22 @@ export class InfraService {
   }
 
   async getPublicIp(): Promise<string> {
-    console.log(this.publicIpApiUrl);
     const ret = await this.http.axiosRef
-      .get(this.publicIpApiUrl)
+      .get(this.PUBLIC_IP_API_URL)
       .catch((err) => {
         logger.error(err);
         throw new Error(
-          `Failed to get public IP from '${this.publicIpApiUrl}'`,
+          `Failed to get public IP from '${this.PUBLIC_IP_API_URL}'`,
         );
       });
+    const curPublicIp = ret.data.trim();
+    if (!this.publicIp !== curPublicIp) {
+      this.onPublicIpChanged({ curPublicIp });
+    }
     return ret.data.trim();
+  }
+
+  private async onPublicIpChanged({ curPublicIp }: { curPublicIp: string }) {
+    this.publicIp = curPublicIp;
   }
 }
